bf3f3ab651919d7499be842a75e3fdf35eda515a
Demi Liu <demiliu@uw.edu>
2018-03-09 14:10:20 -0800

    added user control

--- direct18project/ImageSegmentation.py
+++ direct18project/ImageSegmentation.py
@@ -8,7 +8,29 @@ Created on Wed Mar 07 12:20:10 2018
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import segmentation
-from ImagePreprocessing import convert_to_grayscale
+import cv2
+
+
+def convert_to_grayscale(image):
+    
+    """Converting the image to grayscale - 
+    where minimum pixel value is 0.0 and maximum pixel value is 1.0"""
+    
+    # Checking the right data type for the input image
+    assert type(image) == np.ndarray, ('Wrong data type', 'image must be a numpy array')
+    
+    # converting to grayscale
+    dst = np.zeros(image.shape)
+    image_gray = cv2.normalize(image, dst, 0.0, 1.0, cv2.NORM_MINMAX)
+    
+    # plotting the image
+    plt.figure()
+    plt.gray()
+    plt.title('After background correction, grey-scale')
+    plt.imshow(image_gray)
+    plt.colorbar()
+    
+    return image_gray
 
 
 def seg_random_walker(image, marker_threshold):
@@ -21,9 +43,9 @@ def seg_random_walker(image, marker_threshold):
     assert type(image) == np.ndarray, ('Wrong image data type', 'image must be a numpy array')
     
     # Checking that the input image is grayscale and not RGB or something else
-    assert np.max(image) == 1.0, ('Wrong input image type', 'image must be grayscale')
+    assert np.max(image) == 1.0 or 1, ('Wrong input image type', 'image must be grayscale')
     
-    assert np.min(image) == 0.0, ('Wrong input image type', 'image must be grayscale')
+    assert np.min(image) == 0.0 or 0, ('Wrong input image type', 'image must be grayscale')
     
     # Checking the right data type and range for marker threshold value
     assert type(marker_threshold) == float, ('Wrong data type', 'marker threshold must be a float')
@@ -39,20 +61,43 @@ def seg_random_walker(image, marker_threshold):
     segmented = segmentation.random_walker(image, markers)
     
     # Plotting the segemented, original image and markers
-    plt.figure(dpi = 300)
+    plt.figure()
     seg_image = convert_to_grayscale(segmented)
     plt.title('Segmented Image')
 
-    plt.figure(dpi = 300)
+    plt.figure()
     plt.imshow(image, cmap='gray', interpolation='nearest')
     plt.colorbar()
     plt.title('Original Image')
     
-    plt.figure(dpi = 300)
+    plt.figure()
     plt.imshow(markers, cmap='plasma', interpolation='nearest')
     plt.colorbar()
     plt.title('Markers')
 
        
     return seg_image
+ 
+
+def seg(bckgrnd_corr_image):
+    """Perform and optimize segmentation of the image"""
     
+    seg_ok = 'N'
+    while seg_ok == 'N':
+        print ('Please choose a threshold for segmentation from 0.0 to 1.0')
+        default_threshold = input('Default dimensions? Y/N')    
+            
+        if default_threshold == 'Y':            
+            threshold = 0.15
+    
+        else:
+            threshold = input('Threshold (0.0 to 1.0): ')
+        
+        image_segmentation = seg_random_walker(bckgrnd_corr_image, threshold)
+        
+        seg_ok = input('Are you okay with the segmentation?')
+    
+    return image_segmentation
+
+
+
